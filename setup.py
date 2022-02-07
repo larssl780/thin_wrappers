d6aeda5f7b58d365f38382acfda28b2ca24307d4
Lars <lars.larsson@gmail.com>
2022-02-07 16:47:37 +0000

    Update setup.py
    Increment to include date_range and uk holidays.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="thin_wrappers",
-    version="0.4.1",
+    version="0.4.2",
     description="Thin wrappers around common packages",
     long_description=README,
     long_description_content_type="text/markdown",
