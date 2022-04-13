7e8f6427ca7ad3b9d512e2d303c53022ab2223c8
Lars <lars.larsson@gmail.com>
2022-04-13 11:04:18 +0100

    Add file_finder and format_time

--- thin_wrappers/utils.py
+++ thin_wrappers/utils.py
@@ -12,6 +12,7 @@ import bisect
 import re
 import numpy as np
 import plotly.express as pex
+import math
 try:
     import numpy_ext as npext
 except ImportError:
@@ -324,6 +325,8 @@ def plot_df(df, plot_these=None, normalize=True, title=None, yaxis_title=None):
     assume the df has a proper datetime index (any way to check?)
     if plot_these is None, plot all but the index, m'kay?
     """
+    if df.index.name is None:
+        df.index.name = 'date'
     idx_name = df.index.name
 
     df = df.copy()
@@ -353,7 +356,7 @@ def plot_df(df, plot_these=None, normalize=True, title=None, yaxis_title=None):
 def rolling_apply(func, window, *arrays, n_jobs=1, **kwargs):
     if npext is not None:
         return np.ext.rolling_apply(func, window, *arrays, n_jobs=n_jobs, **kwargs)
-    
+
     return _rolling_apply(func, window, *arrays, n_jobs=n_jobs, **kwargs)
 
 
@@ -561,3 +564,61 @@ def nans(shape, dtype=np.float64):
     arr = np.empty(shape, dtype=dtype)
     arr.fill(np.nan)
     return arr
+
+
+def filter_nans(x):
+    return x[~np.isnan(x)]
+
+
+def format_time(timespan, precision=3):
+    """Formats the timespan in a human readable form (copied from ipython code)"""
+
+    if timespan >= 60.0:
+        # we have more than a minute, format that in a human readable form
+        # Idea from http://snipplr.com/view/5713/
+        parts = [("d", 60 * 60 * 24), ("h", 60 * 60), ("min", 60), ("s", 1)]
+        time = []
+        leftover = timespan
+        for suffix, length in parts:
+            value = int(leftover / length)
+            if value > 0:
+                leftover = leftover % length
+                time.append(u'%s%s' % (str(value), suffix))
+            if leftover < 1:
+                break
+        return " ".join(time)
+
+    # Unfortunately the unicode 'micro' symbol can cause problems in
+    # certain terminals.
+    # See bug: https://bugs.launchpad.net/ipython/+bug/348466
+    # Try to prevent crashes by being more secure than it needs to
+    # E.g. eclipse is able to print a µ, but has no sys.stdout.encoding set.
+    units = [u"s", u"ms", u'us', "ns"]  # the save value
+    if hasattr(sys.stdout, 'encoding') and sys.stdout.encoding:
+        try:
+            u'\xb5'.encode(sys.stdout.encoding)
+            units = [u"s", u"ms", u'\xb5s', "ns"]
+        except:
+            pass
+    scaling = [1, 1e3, 1e6, 1e9]
+
+    if timespan > 0.0:
+        order = min(-int(math.floor(math.log10(timespan)) // 3), 3)
+    else:
+        order = 3
+    return u"%.*g %s" % (precision, timespan * scaling[order], units[order])
+
+
+def file_finder(folder, pattern, case=False, return_last_file=False):
+
+    orig_files = os.scandir(folder)
+    case_flag = re.IGNORECASE if not case else 0
+
+    files = [(folder + x.name, x.stat().st_mtime)
+             for x in orig_files if re.search(pattern, x.name, case_flag) is not None]
+
+    df = pd.DataFrame({'file': [x[0] for x in files], "time": [datetime.datetime.fromtimestamp(
+        x[1]) for x in files]}, columns=['file', 'time']).sort_values('time')
+    if return_last_file:
+        return df.iloc[-1].file
+    return df
